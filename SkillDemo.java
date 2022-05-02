970d20649a4aa5e716028ec622b91319f76d7ef4
jezebelatucsd <103277481+jezebelatucsd@users.noreply.github.com>
2022-05-01 19:08:43 -0700

    added test file

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,5 +1,8 @@
 public class SkillDemo {
 
 //add method 
+public static int div (int n, int d) {
+    return n/d;
+    }
 
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+
+    @Test
+    public void addition() {
+        assertEquals(5, SkillDemo.div(6, 2)); //wrong test 
+    }
+    
+}
